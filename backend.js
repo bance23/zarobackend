a9465b4e2666475866dc08da9ec0df421fded749
bance23 <bancecode@gmail.com>
2022-11-29 11:39:20 +0100

    Mai vegso
    asd

--- backend.js
+++ backend.js
@@ -1,36 +1,37 @@
+const fs = require('fs');
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 const port = 3000
+var path = require('path');
 
-app.use(express.static('kepek'))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+function bufferFile(fileName) {
+    return fs.readFileSync(path.join(__dirname, fileName)); 
+}
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'colorball'
 })
 
-function ido() {
-  var date = new Date().getDate();
-  var month = new Date().getMonth() + 1;
-  var year = new Date().getFullYear();
-
-  return year + '-' + month + '-' + date
-}
+/*const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 's_60db'
+})*/
 
 
 //------------------------     játékos adatok lekérdezése
 app.get('/player', (req, res) => {
-    const mysql = require('mysql')
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'colorball'
-    })
-    
     connection.connect()
     
-    connection.query('SELECT * from player', (err, rows, fields) => {
+    connection.query('SELECT * from test', (err, rows, fields) => {
       if (err) throw err
     
       console.log(rows)
@@ -41,23 +42,15 @@ app.get('/player', (req, res) => {
   })
 
 //-----------------------------------------  player felvitel
-  app.post('/newplayer', (req, res) => {
-    const mysql = require('mysql')
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'colorball'
-    })
+  app.post('/newplayer', (req, res) => {   
+    let username = bufferFile("adat.txt");
+    console.log("username: " + username);
     
-    let datum = ido()
-    console.log(datum)
     connection.connect()
-    
-    connection.query('insert into player values (null, "'+req.body.bevitel1+'", CURDATE())', (err, rows, fields) => {
+    connection.query(`insert into test values (null, '`+username+`', CURDATE())`, (err, rows, fields) => {
       if (err) throw err
     
-      res.send("Sikerült a felvitel!")
+      res.send("Sikerült a felvitel! ")
     })
     
     connection.end()
@@ -66,24 +59,28 @@ app.get('/player', (req, res) => {
 
   //-----------------------------------------  score felvitel
   app.post('/newscore', (req, res) => {
-    const mysql = require('mysql')
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'colorball'
-    })
-    
+
+    let newscore = bufferFile("adat.txt");
+    console.log("newscore: " + newscore);
+
+    let tomb = newscore.split(' ');
+
+    let playerid = tomb[0]
+    let palyaid = tomb[1]
+    let points = tomb[2]
+    let playtime = tomb[3]
+
+
     connection.connect()
-    
-    connection.query('insert into score values (null, '+req.body.bevitel1+', '+req.body.bevitel2+', '+req.body.bevitel3+', CURDATE(), "'+req.body.bevitel4+'")', (err, rows, fields) => {
+    connection.query('insert into score values (null, '+playerid+', '+palyaid+', '+points+', CURDATE(), "'+playtime+'")', (err, rows, fields) => {
       if (err) throw err
     
-      res.send("Sikerült a felvitel!")
+      res.send("Sikerült a felvitel! ")
     })
     
     connection.end()
   })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
