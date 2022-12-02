75f9cca675c10930dd94db854de32076dc839755
balogh <90307762+bance23@users.noreply.github.com>
2022-12-02 10:12:40 +0100

    Update
    Az uj palyak felvitele illetve adott palyan a toplista vegpont

--- backend.js
+++ backend.js
@@ -93,6 +93,51 @@ app.post('/newscore', (req, res) => {
 })
 
 
+//-----------------------------------------  score felvitel
+app.post('/newpalya', (req, res) => {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'colorball'
+  })
+
+  let palya_name = bufferFile("ppalyanev.txt");
+
+  connection.connect()
+
+  connection.query('insert into palya values (null, ' + palya_name + ')', (err, rows, fields) => {
+    if (err) throw err
+
+    res.send("Sikerült a felvitel! ")
+  })
+
+  connection.end()
+})
+
+app.get('/toplist', (req, res) => {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'colorball'
+  })
+  connection.connect()
+
+  //let palya_id = bufferFile("ppalyaid.txt");
+
+  connection.query('SELECT player.player_name, score.score_points, score.score_time FROM `score` INNER JOIN player ON player.player_id = score.score_playerid WHERE score.score_palyaid = ' + req.body.bevitel1 + ' ORDER BY score.score_points DESC LIMIT 10', (err, rows, fields) => {
+    if (err) throw err
+
+    console.log(rows)
+    res.send(rows)
+  })
+
+  connection.end()
+})
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
